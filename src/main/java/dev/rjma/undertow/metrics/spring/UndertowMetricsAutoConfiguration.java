b5261c0030751a8c1048596213d9ee3ebeb1663b
Rupert Madden-Abbott <rupert@madden-abbott.com>
2025-03-18 21:35:13 +0000

    Add skeleton autoconfiguration class.

---
+++ src/main/java/dev/rjma/undertow/metrics/spring/UndertowMetricsAutoConfiguration.java
@@ -0,0 +1,8 @@
+package dev.rjma.undertow.metrics.spring;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+
+@AutoConfiguration
+public class UndertowMetricsAutoConfiguration {
+
+}
---
+++ src/test/java/dev/rjma/undertow/metrics/spring/UndertowMetricsAutoConfigurationTest.java
@@ -0,0 +1,18 @@
+package dev.rjma.undertow.metrics.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+
+class UndertowMetricsAutoConfigurationTest {
+
+    private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(UndertowMetricsAutoConfiguration.class));
+
+    @Test
+    void shouldLoadAutoConfiguration() {
+        contextRunner.run(context -> {
+            context.getBean(UndertowMetricsAutoConfiguration.class);
+        });
+    }
+}
