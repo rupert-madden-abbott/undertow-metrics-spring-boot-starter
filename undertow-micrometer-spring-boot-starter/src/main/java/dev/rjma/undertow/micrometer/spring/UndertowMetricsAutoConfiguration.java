f5386d427ca4e2ef31fe6496f7d1fe73a0da49cb
Rupert Madden-Abbott <rupert@madden-abbott.com>
2025-03-22 10:21:21 +0000

    Enable undertow metrics.

--- undertow-micrometer-spring-boot-starter/src/main/java/dev/rjma/undertow/micrometer/spring/UndertowMetricsAutoConfiguration.java
+++ undertow-micrometer-spring-boot-starter/src/main/java/dev/rjma/undertow/micrometer/spring/UndertowMetricsAutoConfiguration.java
@@ -1,8 +1,14 @@
 package dev.rjma.undertow.micrometer.spring;
 
+import io.undertow.server.handlers.MetricsHandler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.web.embedded.undertow.UndertowDeploymentInfoCustomizer;
+import org.springframework.context.annotation.Bean;
 
 @AutoConfiguration
 public class UndertowMetricsAutoConfiguration {
-
+    @Bean
+    UndertowDeploymentInfoCustomizer undertowDeploymentInfoCustomizer() {
+        return deploymentInfo -> deploymentInfo.addOuterHandlerChainWrapper(MetricsHandler.WRAPPER);
+    }
 }
